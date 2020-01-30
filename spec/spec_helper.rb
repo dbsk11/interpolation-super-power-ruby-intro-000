ffb90acdbdd27ead35c597de9bbaf9dbda1c4b11
balanced-widget-4002 <jessikim807@gmail.com>
2020-01-30 02:20:19 +0000

    Automatically backed up by Learn

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,5 +3,3 @@ require_relative "../lib/display_rainbow.rb"
 RSpec.configure do |config|
   RSpec::Expectations.configuration.warn_about_potential_false_positives = false
 end
-
-colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
